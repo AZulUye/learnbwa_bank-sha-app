0d1aa125186d03609a51a00b15b6fb115be85cf0
Alif Zulkifly <mzalfirdausy1101@gmail.com>
2023-03-23 20:40:01 +0700

    Day 1

---
+++ lib/ui/page/splash_page.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:banksha/shared/theme.dart';
+import 'package:banksha/ui/page/onboarding_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/img_logo_dark.png',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
