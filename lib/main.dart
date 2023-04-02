592de431b96a97277e2a60e2e7e74e049bd828ea
Alif Zulkifly <mzalfirdausy1101@gmail.com>
2023-04-02 16:59:20 +0700

    Start Consume API

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:banksha/bloc/auth/auth_bloc.dart';
 import 'package:banksha/shared/theme.dart';
 import 'package:banksha/ui/page/data_package_page.dart';
 import 'package:banksha/ui/page/data_provider_page.dart';
@@ -21,9 +24,20 @@ import 'package:banksha/ui/page/topup_success_page.dart';
 import 'package:banksha/ui/page/transfer_amount_page.dart';
 import 'package:banksha/ui/page/transfer_page.dart';
 import 'package:banksha/ui/page/transfer_success_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -33,47 +47,54 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: lightBackgroundColor,
-        appBarTheme: AppBarTheme(
-          backgroundColor: lightBackgroundColor,
-          elevation: 0,
-          centerTitle: true,
-          iconTheme: IconThemeData(
-            color: blackColor,
-          ),
-          titleTextStyle: blackTextStyle.copyWith(
-            fontSize: 20,
-            fontWeight: semiBold,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: lightBackgroundColor,
+          appBarTheme: AppBarTheme(
+            backgroundColor: lightBackgroundColor,
+            elevation: 0,
+            centerTitle: true,
+            iconTheme: IconThemeData(
+              color: blackColor,
+            ),
+            titleTextStyle: blackTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
           ),
         ),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/onboarding': (context) => const OnboardingPage(),
+          '/sign-in': (context) => const SignInPage(),
+          '/sign-up': (context) => const SignUpPage(),
+          '/sign-up-set-profile': (context) => const SignUpSetProfilePage(),
+          '/sign-up-set-ktp': (context) => const SignUpSetKTPPage(),
+          '/sign-up-success': (context) => const SignUpSuccessPage(),
+          '/home': (context) => const HomePage(),
+          '/profile': (context) => const ProfilePage(),
+          '/pin': (context) => const PinPage(),
+          '/profile-edit': (context) => const ProfileEditPage(),
+          '/profile-edit-pin': (context) => const ProfileEditPinPage(),
+          '/profile-edit-success': (context) => const ProfileEditSuccessPage(),
+          '/topup': (context) => const TopupPage(),
+          '/topup-amount': (context) => const TopupAmountPage(),
+          '/topup-success': (context) => const TopupSuccessPage(),
+          '/transfer': (context) => const TransferPage(),
+          '/transfer-amount': (context) => const TransferAmountPage(),
+          '/transfer-success': (context) => const TransferSuccessPage(),
+          '/data-provider': (context) => const DataProviderPage(),
+          '/data-package': (context) => const DataPackagePage(),
+          '/data-success': (context) => const DataSuccessPage(),
+        },
       ),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/onboarding': (context) => const OnboardingPage(),
-        '/sign-in': (context) => const SignInPage(),
-        '/sign-up': (context) => const SignUpPage(),
-        '/sign-up-set-profile': (context) => const SignUpSetProfilePage(),
-        '/sign-up-set-ktp': (context) => const SignUpSetKTPPage(),
-        '/sign-up-success': (context) => const SignUpSuccessPage(),
-        '/home': (context) => const HomePage(),
-        '/profile': (context) => const ProfilePage(),
-        '/pin': (context) => const PinPage(),
-        '/profile-edit': (context) => const ProfileEditPage(),
-        '/profile-edit-pin': (context) => const ProfileEditPinPage(),
-        '/profile-edit-success': (context) => const ProfileEditSuccessPage(),
-        '/topup': (context) => const TopupPage(),
-        '/topup-amount': (context) => const TopupAmountPage(),
-        '/topup-success': (context) => const TopupSuccessPage(),
-        '/transfer': (context) => const TransferPage(),
-        '/transfer-amount': (context) => const TransferAmountPage(),
-        '/transfer-success': (context) => const TransferSuccessPage(),
-        '/data-provider': (context) => const DataProviderPage(),
-        '/data-package': (context) => const DataPackagePage(),
-        '/data-success': (context) => const DataSuccessPage(),
-      },
     );
   }
 }
--- lib/ui/page/sign_up_page.dart
+++ lib/ui/page/sign_up_page.dart
@@ -1,94 +1,149 @@
+import 'package:banksha/bloc/auth/auth_bloc.dart';
+import 'package:banksha/shared/shared_methods.dart';
 import 'package:banksha/shared/theme.dart';
 import 'package:banksha/ui/widget/button.dart';
 import 'package:banksha/ui/widget/form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final nameController = TextEditingController(text: '');
+  final emailController = TextEditingController(text: '');
+  final passwordController = TextEditingController(text: '');
+
+  bool validate() {
+    if (nameController.text.isEmpty ||
+        emailController.text.isEmpty ||
+        passwordController.text.isEmpty) {
+      return false;
+    }
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        children: [
-          Container(
-            width: 155,
-            height: 50,
-            margin: const EdgeInsets.only(
-              top: 100,
-              bottom: 100,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          // TODO: implement listener
+
+          if (state is AuthFailed) {
+            showCustomSnackBar(context, state.e);
+          }
+
+          if (state is AuthCheckEmailSuccess) {
+            Navigator.pushNamed(context, '/sign-up-set-profile');
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/img_logo_light.png',
+            children: [
+              Container(
+                width: 155,
+                height: 50,
+                margin: const EdgeInsets.only(
+                  top: 100,
+                  bottom: 100,
+                ),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/img_logo_light.png',
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          Text(
-            'Join Us to Unlock\nYour Growth',
-            style: blackTextStyle.copyWith(
-              fontSize: 20,
-              fontWeight: semiBold,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            padding: const EdgeInsets.all(22),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: whiteColor,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                //NOTE: FULLNAME INPUT
-                CustomFormField(
-                  title: 'Full Name',
+              Text(
+                'Join Us to Unlock\nYour Growth',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
                 ),
-                const SizedBox(
-                  height: 16,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: whiteColor,
                 ),
-                //NOTE: EMAIL INPUT
-                CustomFormField(
-                  title: 'Email Address',
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //NOTE: FULLNAME INPUT
+                    CustomFormField(
+                      title: 'Full Name',
+                      controller: nameController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    //NOTE: EMAIL INPUT
+                    CustomFormField(
+                      title: 'Email Address',
+                      controller: emailController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    //NOTE: PASSWORD INPUT
+                    CustomFormField(
+                      title: 'Password',
+                      controller: passwordController,
+                      obscureText: true,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    //TODO: Register Button Logic
+                    CustomFilledButton(
+                      title: 'Continue',
+                      onPressed: () {
+                        if (validate()) {
+                          context
+                              .read<AuthBloc>()
+                              .add(AuthCheckEmail(emailController.text));
+                        } else {
+                          showCustomSnackBar(
+                              context, 'Semua field harus diisi');
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 16,
-                ),
-                //NOTE: PASSWORD INPUT
-                CustomFormField(
-                  title: 'Password',
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomFilledButton(
-                  title: 'Continue',
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/sign-up-set-profile');
-                  },
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomTextButton(
-            title: 'Sign In',
-            onPressed: () {
-              Navigator.pushNamed(context, '/sign-in');
-            },
-          ),
-        ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              CustomTextButton(
+                title: 'Sign In',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/sign-in');
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
